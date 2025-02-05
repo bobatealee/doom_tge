d3dd1edf6f7189b363afbdce520a8e196bcf3755
Boba <trashboyjet@gmail.com>
2025-02-05 14:55:12 -0600

    1.2.4

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,32 @@
 runOnStartup(runtime => {
+	// block certain keys
 	window.addEventListener("keydown", function(e) {
 		if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
 			e.preventDefault();
 		}
 	}, false);
+
+	var style = document.createElement("style");
+	style.innerText =`
+	/* border texture */
+	body:before {
+		background: url(border.png);
+		content: "";
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		opacity: 0;
+		animation: fadeBackground 0.5s forwards;
+	}
+
+	/* border frame */
+	canvas {
+		box-shadow: 0px 0px 0px calc(2px * var(--construct-scale)) #566066, 0px 0px calc(5px * var(--construct-scale)) calc(2px * var(--construct-scale)) #000000;
+		background: #000000;
+		opacity: 0;
+		animation: fadeBorder 0.25s forwards;
+	}
+	`;
+
+	document.head.appendChild(style);
 });
